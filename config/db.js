dfb8a0f84bf2449f4f26819570baf8be458ef0da
semiartist <semiartist@ymail.com>
2024-03-05 21:46:21 -0600

    update deploy server URI

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-	const conn = await mongoose.connect(process.env.MONGO_URI);
+	const conn = await mongoose.connect(process.env.MONGO_URI_DEPLOY);
 	console.log(`Mongo DB connected, ${conn.connection.host}`);
 };
 
