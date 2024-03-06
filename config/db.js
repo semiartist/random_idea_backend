1e2805850023943ad1bdf3c664aba687e1578e1c
semiartist <semiartist@ymail.com>
2024-03-06 16:01:15 -0600

    build version

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	// production script
-	// const conn = await mongoose.connect(process.env.MONGO_URI_DEPLOY);
+	const conn = await mongoose.connect(process.env.MONGO_URI_DEPLOY);
 	//dev script
-	const conn = await mongoose.connect(process.env.MONGO_URI);
+	// const conn = await mongoose.connect(process.env.MONGO_URI);
 	console.log(`Mongo DB connected, ${conn.connection.host}`);
 };
 
